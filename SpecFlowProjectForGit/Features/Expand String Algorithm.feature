c3246973b4408820b54011c0d0725671938394da
GAVIREDDY Siva Mohan Reddy <smr.gavireddy@soprasteria.com>
2023-05-22 11:34:08 +0530

    second commit

---
+++ SpecFlowProjectForGit/Features/Expand String Algorithm.feature
@@ -0,0 +1,9 @@
+﻿Feature: Expand String Algorithm
+
+A short summary of the feature
+
+@tag1
+Scenario: Print string 
+	Given the given string is 'a2b10'
+	When check the string having characters and the numbers as a repeats 
+	Then create string based on the string character repeats  
--- SpecFlowProjectForGit/StepDefinitions/CalculatorStepDefinitions.cs
+++ SpecFlowProjectForGit/StepDefinitions/CalculatorStepDefinitions.cs
@@ -4,41 +4,47 @@ namespace SpecFlowProjectForGit.StepDefinitions
     public sealed class CalculatorStepDefinitions
     {
         // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
-
+        int input1 = 0;
+        int input2 = 0;
+        int output1 = 0;
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            throw new PendingStepException();
+             input1=number;
         }
-
+        
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-
-            throw new PendingStepException();
+             input2=number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
-            throw new PendingStepException();
+            
+            output1 = input1 + input2;
         }
 
         [Then("the result should be (.*)")]
         public void ThenTheResultShouldBe(int result)
         {
-            //TODO: implement assert (verification) logic
-
-            throw new PendingStepException();
+            try
+            {
+                if(output1 == result)
+                {
+                    Console.WriteLine("the output "+output1+" and results "+result+" is same ");
+                }
+                else
+                {
+                    Console.WriteLine("failed adding two numbers");
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                throw new Exception();
+            }
         }
     }
 }
